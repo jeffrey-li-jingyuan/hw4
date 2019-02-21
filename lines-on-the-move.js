b2c6b2d0206baf2cf52226ed148aa336a02dcba4
jeffrey-li-jingyuan <47227251+jeffrey-li-jingyuan@users.noreply.github.com>
2019-02-21 14:26:36 -0800

    Update lines-on-the-move.js

--- lines-on-the-move.js
+++ lines-on-the-move.js
@@ -1,1 +1,23 @@
+var m = []
+function setup() {
+  createCanvas(400, 400);
+  colorMode(HSB);
+  frameRate(24)
+  for (i = 0; i < 1000; i++){
+	var marching = {
+	x: random(width),
 
+  }
+  m.push(marching)
+	}
+}
+
+
+function draw() {
+  background(255);
+  
+  for (var i = 100; i <width; i++) {
+    stroke(random(255))
+    line(m[i].x, height / 2, m[i].x + 100, height / 2 - 75);
+  }
+}
